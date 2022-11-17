8c2daf971187821f3e4140130d73b93468f35828
tielorT <111721927+tielorT@users.noreply.github.com>
2022-11-17 06:01:16 -0600

    commit

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link href="https://fonts.googleapis.com/css2?family=Kanit&display=swap" rel="stylesheet" />
-        <script src="https://kit.fontawesome.com/b1fb33b38c.js" crossOrigin="anonymous"></script>
+        <script src="https://kit.fontawesome.com/b1fb33b38c.js" crossOrigin="anonymous" async />
       </Head>
       <body>
         <Main />
