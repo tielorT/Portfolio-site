4c36f507c283098044d1ce0ce4b5fbc1ee7a9596
tielorT <111721927+tielorT@users.noreply.github.com>
2022-11-17 05:47:55 -0600

    commit

---
+++ components/Projects.js
@@ -0,0 +1,62 @@
+import styles from '../styles/Projects.module.css'
+import { IconContext } from "react-icons";
+import Image from 'next/image';
+import img1 from '../public/untitled.png'
+import img2 from '../public/img2.png'
+import { SiGithub } from 'react-icons/si'
+import { BiLinkExternal } from 'react-icons/bi'
+
+const Projects = () => {
+    return (
+        <div className={styles.container}>
+            <h2>My Projects</h2>
+            <div style={{ width: '100%'}}>
+                <div className={styles.projectContent}>
+                    <div className={styles.project}>
+                        <div>
+                            <h4>Mexican Restaurant Site</h4>
+                            <p>A Restaurant site where users can add porducts to a cart.
+                                Utilizing nextjs/react, mongodb, and aws to create a functioning Website.
+                            </p>
+                            <div>
+                                <SiGithub />
+                            </div>
+                        </div>
+                        <div style={{ position: 'relative'}}>
+                            <Image  className={styles.img} src={img1} width={400} height={300}/>
+                            <IconContext.Provider value={{ size: '50px'}}>
+                            <div className={styles.externalLink}>
+                                <BiLinkExternal style={{ size: '90px'}}/>
+                            </div>
+                            </IconContext.Provider>
+                        </div>
+                    </div>
+                    <div className={styles.project}>
+                        <div>
+                            <h4>Favourite Sweets Website</h4>
+                            <p>A Website to showcase my front-end abilities while 
+                               showing you my Favourite sweets to eat! project
+                               uses React/Nextjs bootstrap, and html/css.  </p>
+                            <div>
+                                <SiGithub />
+                            </div>   
+                        </div>
+                        <div style={{ position: 'relative'}}>
+                        <Image  className={styles.img} src={img2} width={400} height={300}/>
+                        <IconContext.Provider value={{ size: '50px'}}>
+                            <div className={styles.externalLink}>
+                                <BiLinkExternal style={{ size: '90px'}}/>
+                            </div>
+                            </IconContext.Provider>
+                        </div>
+                    </div>
+                </div>
+                <div>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Projects;
--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,13 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import About from '../components/About';
+import Projects from '../components/Projects';
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <About />
+      <Projects />
     </div>
   )
 }
